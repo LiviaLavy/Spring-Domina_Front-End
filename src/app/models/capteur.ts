02c5893c3c4a96da45d411e2da69f8ef69c78c1b
Lavy Livia <lavy.livia@gmail.com>
2019-05-03 12:33:13 +0200

    models bases

---
+++ src/app/models/capteur.ts
@@ -0,0 +1,9 @@
+import { PipeCollector } from '@angular/compiler/src/template_parser/binding_parser';
+
+export interface Capteur {
+    idCapteur: number;
+    type: string;
+    piece: Piece;
+    
+
+}
---
+++ src/app/models/piece.ts
@@ -0,0 +1,7 @@
+import { Programme } from './programme';
+export class Piece {
+idPiece: number;
+nomPiece: string;
+superficieM2: number;
+programmes: Programme[];
+}
